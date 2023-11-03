15326edc80d070d9c405765feb08c72a7e62686a
Tad Decker <trdecker@byu.edu>
2023-11-03 15:42:25 -0600

    start.js

--- app.js
+++ app.js
@@ -14,6 +14,6 @@ const app = express()
 app.use(express.json())
 app.use('/', apiRoutes)
 
-// app.listen(port, () => {
-//     console.log(`Server is listening on port ${port}`)
-// })
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`)
+})
